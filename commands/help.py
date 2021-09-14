61ac552646945b00d7fc7fb9a35da2cae1126803
Ricardo Antonio <ricardoantoniomx@gmail.com>
2021-09-13 19:58:04 -0500

    Ranking update schedule added to help

--- commands/help.py
+++ commands/help.py
@@ -5,6 +5,7 @@ def help(update, context):
     '\n\n/ranking - Muestra el ranking'
     '\n/agregar <i>NOMBRE DE LA EMPRESA</i> - Agrega tu empresa a la lista'
     '\n/eliminar <i>NOMBRE DE LA EMPRESA</i> - Elimina tu empresa de la lista'
-    '\n\n<i>En todos los casos donde se pide el <b>NOMBRE DE LA EMPRESA,</b> es necesario que la escribas <b>tal como aparece en el juego.</b> Lo mejor es copiarla de tu perfil. Recuerda dejar un espacio entre el comando y el nombre de tu empresa.</i>')
+    '\n\n<i>En todos los casos donde se pide el <b>NOMBRE DE LA EMPRESA,</b> es necesario que la escribas <b>tal como aparece en el juego.</b> Lo mejor es copiarla de tu perfil. Recuerda dejar un espacio entre el comando y el nombre de tu empresa.</i>'
+    '\n\n<b>El ranking se actualiza todos los días a las <a href="https://time.is/es/UTC">02:00 UTC</a>.</b>')
     
-    context.bot.send_message(chat_id=update.effective_chat.id, text=msg, parse_mode=PARSEMODE_HTML)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg, parse_mode=PARSEMODE_HTML, disable_web_page_preview=True)
