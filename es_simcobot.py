ffc29941a3ee416937c65c11d2db221bc64f1c03
Ricardo Antonio <ricardoantoniomx@gmail.com>
2021-09-22 16:05:28 -0500

    Send message when new SCTimes edition is available

--- es_simcobot.py
+++ es_simcobot.py
@@ -1,11 +1,12 @@
 import logging
 from telegram.ext import Updater, CommandHandler
-from telegram import ParseMode
 from decouple import config
 from commands.addcompany import add_company
 from commands.delcompany import del_company
 from commands.ranking import ranking
 from commands.help import help
+from features.simcotimes import get_simco_times
+from apscheduler.schedulers.background import BackgroundScheduler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -38,6 +39,13 @@ def main():
 
     # Start the bot
     updater.start_polling()
+
+    # Tareas programadas
+    GROUP_ID = config('GROUPID')
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(get_simco_times, 'cron', args=[updater, GROUP_ID], day_of_week='wed', hour=21, minute=10,
+                      timezone='UTC')
+    scheduler.start()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
---
+++ features/simcotimes.py
@@ -0,0 +1,58 @@
+import os
+import sqlite3
+from urllib.request import urlopen
+import urllib.error
+import json
+import ssl
+from time import sleep
+from telegram.constants import PARSEMODE_HTML
+
+
+def get_simco_times(updater, chat_id):
+    print('Buscando SimCompanies Times')
+
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(os.path.join(dir_path, '../commands/simcobot.db'))
+    cur = conn.cursor()
+    cur.execute('''SELECT value FROM configurations WHERE parameter="sctimes_edition"''')
+    edition = cur.fetchone()[0]
+    conn.close()
+    print(edition)
+
+    # Conexion a la API
+    service_url = 'https://www.simcompanies.com/api/v3/es/newspaper/'
+    # Ignorar errores de certificado SSL
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    url = service_url + str(edition)
+
+    js = None
+
+    while not js:
+        try:
+            uh = urlopen(url, context=ctx)
+            datos = uh.read().decode()
+        except urllib.error.HTTPError as err:
+            if err.code == 404:
+                datos = None
+        try:
+            js = json.loads(datos)
+        except:
+            js = None
+        if js: break
+        sleep(60)
+
+    msg = '<b>NUEVA EDICIÓN DE SIMCOMPANIES TIMES</b>\n\n'.format(edition)
+    for i, article in enumerate(js['articles']):
+       msg += '<b>{}. {}</b>\n{}\n'.format(i+1, article['title'], article['copy1'][:100]+'...')
+    msg += '\n\nhttps://www.simcompanies.com/es/newspaper/{}'.format(edition)
+
+    updater.bot.sendMessage(chat_id=chat_id, text=msg, parse_mode=PARSEMODE_HTML)
+
+    conn = sqlite3.connect(os.path.join(dir_path, '../commands/simcobot.db'))
+    cur = conn.cursor()
+    cur.execute('''UPDATE configurations SET value = ?''', (edition + 1,))
+    conn.commit()
+    conn.close()
