4df1f9021f18c53f6e9fb9952d4a79606efbd740
Ricardo Antonio <ricardoantoniomx@gmail.com>
2021-10-23 10:23:41 -0500

    Updated welcome messages

--- features/newmembers.py
+++ features/newmembers.py
@@ -10,6 +10,8 @@ def new_member(updater, context):
         msg = f"👤 <b>{member.full_name}</b> se ha unido al grupo."
         if member['is_bot']:
             msg += '\n️⚠️ Es probable que se trate de un bot.\n¿Alguien puede verificar?'
+        else:
+            msg += '¿Ya le dieron la bienvenida?'
 
     try:
         context.bot.send_message(chat_id=updater.effective_chat.id, text='¡Hola, {}! Bienvenid@ a <b><i>Sim Companies ES</i></b> 🇲🇽🇨🇱🇪🇸🇨🇴🇨🇺🇪🇨🇩🇴🇬🇶🇸🇻🇬🇹🇭🇳🇳🇮🇵🇦🇵🇾🇵🇪🇵🇷🇺🇾🇻🇪🇦🇷\n\n"Antes que nada por favor presentate con un nombre y el nombre de tu empresa, ya que unicamente miembros activos y reales pueden quedarse."\n\nDiviertanse Amig@s 😄 Recuerden Leer las /reglas y Para Más Información Pon /info'.format(member.full_name), parse_mode=PARSEMODE_HTML)
@@ -19,4 +21,4 @@ def new_member(updater, context):
     try:
         context.bot.send_message(chat_id=chat_id, text=msg, parse_mode=PARSEMODE_HTML)
     except:
-        print('No se pudo enviar el mensaje')
+        print('No se pudo enviar el mensaje de aviso al log')
