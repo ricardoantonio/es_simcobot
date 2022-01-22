58a2bda96cb368012121d694f1ef0fe06a0c27f9
Ricardo Antonio <ricardoantoniomx@gmail.com>
2022-01-22 11:16:29 -0600

    Founder name changed

--- commands/info.py
+++ commands/info.py
@@ -9,6 +9,6 @@ def info(update, context):
     '\n\nSimbloteca'
     '\n@Simbloteca'
 
-    '\n\nCreador del Grupo: @EHDTM_SC')
+    '\n\nCreador del Grupo: @GrupoPetroMex_SC')
     
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg, parse_mode=PARSEMODE_HTML, disable_web_page_preview=True)
