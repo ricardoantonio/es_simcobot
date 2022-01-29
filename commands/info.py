65543744a59754c177d832336010a09ae03edc83
Ricardo Antonio <ricardoantoniomx@gmail.com>
2022-01-29 11:55:21 -0600

    Update Infor

--- commands/info.py
+++ commands/info.py
@@ -2,13 +2,13 @@ from telegram.constants import PARSEMODE_HTML
 
 def info(update, context):
     msg = ('<b>Sim Companies Comunidad en Español</b>'
-    '\nCanales de Información: Chat Libre, Youtube y Facebook:'
+    '\nChat General:'
+    '\nhttps://t.me/+7e9LQzWvos9hNWFh'
+    '\nCanal de Información:'
     '\n@SimCompaniesEs'
     '\n\nChat de Ventas:'
     '\n@SCCEChatDeVentas'
     '\n\nSimbloteca'
-    '\n@Simbloteca'
-
-    '\n\nCreador del Grupo: @GrupoPetroMex_SC')
+    '\n@Simbloteca')
     
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg, parse_mode=PARSEMODE_HTML, disable_web_page_preview=True)
